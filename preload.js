38162236221e139f858dfbefce5f725cd0fb4147
M1chU02 <m.mikolajewski02@gmail.com>
2025-09-29 16:25:33 +0200

    first commit

---
+++ preload.js
@@ -0,0 +1,11 @@
+// preload.js
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('api', {
+  listChallenges: () => ipcRenderer.invoke('challenges:list'),
+  createChallenge: (payload) => ipcRenderer.invoke('challenges:create', payload),
+  updateChallenge: (id, patch) => ipcRenderer.invoke('challenges:update', { id, patch }),
+  deleteChallenge: (id) => ipcRenderer.invoke('challenges:delete', id),
+  addLog: (id, dateISO, km, note) => ipcRenderer.invoke('logs:add', { id, dateISO, km, note }),
+  deleteLog: (id, logId) => ipcRenderer.invoke('logs:delete', { id, logId }),
+});
